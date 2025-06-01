448a7a3af6efded01f5c4afb879f947d689a97c3
andrew-jameson <ajameson@teamraft.com>
2025-06-01 13:35:53 -0400

    removed sitename for wider net

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from jobspy import scrape_jobs
 
 def run_scraper():
     jobs = scrape_jobs(
-        site_name="indeed",
         search_term="Python Developer",
         location="Remote",
         results_wanted=20,
